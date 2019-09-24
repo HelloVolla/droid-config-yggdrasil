993039d4fb763b24e153695b88afab7058016c01
NeKit <nekit1000@gmail.com>
2019-09-24 13:08:38 +0200

    Bind-mount patched libmtk-ril.so to fix accepting incoming calls

--- sparse/usr/bin/droid/mount_overlay_system.sh
+++ sparse/usr/bin/droid/mount_overlay_system.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 mkdir -p /system
 mount -t overlay overlay -o lowerdir=/usr/libexec/droid-hybris/system:/system_root/system /system
- 
+
+# override libmtk-ril.so with patched version for accepting incoming calls
+# without MediaTek vendor extensions
+mount -o bind /usr/libexec/droid-hybris/system/lib64/libmtk-ril.so /vendor/lib64/libmtk-ril.so
