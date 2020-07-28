b5ab3ab84c12ea0e2d9f2a4befc2dea38c380a4b
Adam Pigg <adam@piggz.co.uk>
2020-07-28 13:11:02 +0100

    Add patched camera libs from UT halium port.
    This gets the viewfinder working, but taking pictures still causes
    errors.
    
    Change-Id: I10317b74ea24f0fea2e530230f6631aa872b0d4a

--- sparse/usr/bin/droid/mount_overlay_system.sh
+++ sparse/usr/bin/droid/mount_overlay_system.sh
@@ -5,3 +5,8 @@ mount -t overlay overlay -o lowerdir=/usr/libexec/droid-hybris/system:/system_ro
 # override libmtk-ril.so with patched version for accepting incoming calls
 # without MediaTek vendor extensions
 mount -o bind /usr/libexec/droid-hybris/system/lib64/libmtk-ril.so /vendor/lib64/libmtk-ril.so
+
+# Override mtk camera lis with patched versions from halium UT port
+mount -o bind /usr/libexec/droid-hybris/system/lib64/libcameracustom.so /vendor/lib64/libcameracustom.so
+mount -o bind /usr/libexec/droid-hybris/system/lib64/libcam.halsensor.so /vendor/lib64/libcam.halsensor.so
+mount -o bind /usr/libexec/droid-hybris/system/lib64/libmtkcam_metastore.so /vendor/lib64/libmtkcam_metastore.so
