1fe7a1bf04c5c90ba62997708efe5cc21b781c66
Adam Pigg <adam@piggz.co.uk>
2020-10-14 12:44:23 +0100

    Flash logo and lk partitions
    Change-Id: Ia53c51ac79b7f895f11fc4fb14c65e10996c4ac2

--- sparse/boot/flash.sh
+++ sparse/boot/flash.sh
@@ -4,3 +4,8 @@ fastboot flash boot hybris-boot.img
 #Root and home filesystems
 fastboot flash userdata sailfish.img001
 
+#Sailfish Logo
+fastboot flash logo logo.bin
+
+#Lock partition to allow flashing and remove boot warning
+fastboot flash lk lk-yggdrasil.img
