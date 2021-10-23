ec18dda7283e2de56e0564068bcf4cb70271b135
Adam Pigg <adam@piggz.co.uk>
2021-10-23 14:33:55 +0100

    Add flash script for volla-x

---
+++ sparse/boot/flash-vollax.sh
@@ -0,0 +1,12 @@
+#Kernel and init
+fastboot flash boot hybris-boot.img
+
+#Root and home filesystems
+fastboot flash userdata sailfish.img001
+
+#Sailfish Logo
+fastboot flash logo logo.bin
+
+#Lock partition to allow flashing and remove boot warning
+fastboot flash lk lk-yggdrasilx.img
+
