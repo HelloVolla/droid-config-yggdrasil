4b2705d94f347e4102302023d6554c525b897594
Adam Pigg <adam@piggz.co.uk>
2021-10-27 14:44:45 +0100

    Support X/10 BT address file

--- sparse/usr/bin/droid/droid-get-bt-address.sh
+++ sparse/usr/bin/droid/droid-get-bt-address.sh
@@ -4,4 +4,10 @@ while [ "$(/usr/bin/getprop vendor.service.nvram_init)" != "Ready" ]; do
     sleep 1
 done
 
-hexdump -s 0 -n 6 -ve '/1 "%02X:"' /vendor/nvdata/APCFG/APRDEB/BT_Addr | sed "s/:$//g" > /var/lib/bluetooth/board-address
+FILE=/mnt/vendor/nvdata/APCFG/APRDEB/BT_Addr
+if [ -f "$FILE" ]; then
+    hexdump -s 0 -n 6 -ve '/1 "%02X:"' $FILE | sed "s/:$//g" > /var/lib/bluetooth/board-address
+else
+    hexdump -s 0 -n 6 -ve '/1 "%02X:"' /vendor/nvdata/APCFG/APRDEB/BT_Addr | sed "s/:$//g" > /var/lib/bluetooth/board-address
+fi
+
